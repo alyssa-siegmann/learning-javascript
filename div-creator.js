3c6d4e8c8980adf68ebf6b2f54b1d71bae0f8f8a
Alyssa Siegmann <alyssasiegmann@Alyssas-MacBook-Pro.local>
2021-11-14 18:14:07 -0500

    two column div adder

--- div-creator.js
+++ div-creator.js
@@ -1,10 +1,22 @@
-for (let i = 0; i < 10; i++) {
-  let d = document.createElement("div")
-  document.body.append(d)
-  d.innerText = i
-  let str = 'color: white; background-color: #';
-    for (let x = 0; x < 6; x++) {
-    str += i;
-    }
-  d.style = str
+function whiteOut(x) {
+  if (document.getElementById(x).className == "whitedOut") {
+    document.getElementById(x).className = ""
+  } else {
+    document.getElementById(x).className = "whitedOut";
+  }
 }
+
+numExistingDivs = 0
+
+function addADiv() {
+  let d = document.createElement("div")
+  sectionA.append(d)
+  d.innerText = numExistingDivs 
+  d.id = numExistingDivs 
+  d.onclick = function() { whiteOut(d.id) }
+  let str = 'color: white; background-color: #';
+  str += Math.floor(100000 + Math.random() * 900000);
+  d.style = str
+  numExistingDivs = numExistingDivs + 1
+}
+
